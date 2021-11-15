cc7b60b5e3424457af4e7a0e76f356e0a2d19c89
Ryan Fitzgerald <ryfitz@ymail.com>
2021-11-15 13:56:03 -0700

    init

---
+++ planit/client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'fitz-codeworks.us.auth0.com'
+export const clientId = 'ES1ZkmAUi6WwqLMUtOjPRfCPLCPzxInb'
+export const audience = 'https://RyanDev.com'
